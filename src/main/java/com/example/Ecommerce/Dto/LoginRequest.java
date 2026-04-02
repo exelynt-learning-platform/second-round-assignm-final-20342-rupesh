6df10485144218348a134de2f7cd66e0c11c56e0
rupeshpatil2003-dot <rupeshpatil2003@gmail.com>
2026-04-02 20:08:25 +0530

    Reverted branch back to state of 3833a0f

--- src/main/java/com/example/Ecommerce/Dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.Ecommerce.Dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-	private String email;
-	private String password;
-}
--- src/main/java/com/example/Ecommerce/Repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Ecommerce.Repository;
-
-import com.example.Ecommerce.Entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
